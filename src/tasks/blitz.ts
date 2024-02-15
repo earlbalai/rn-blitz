785ed33c9ca58c19e854142a597fc81f4ef22ed8
Earl Balai Jr <earl@kiwi.io>
2024-02-15 10:19:36 +0800

    Fixed command types

--- src/tasks/blitz.ts
+++ src/tasks/blitz.ts
@@ -1,21 +1,21 @@
-import path from "path"
-import inquirer from "inquirer"
-import { execCommand } from "../utils.js"
-import { askSetupOptions } from "./askSetupOptions.js" // Import missing function
-import configureProject from "./configureProject.js" // Import missing function
+import path from "path";
+import inquirer from "inquirer";
+import { execCommand } from "../utils.js";
+import { askSetupOptions } from "./askSetupOptions.js";
+import configureProject from "./configureProject.js";
 import {
   askPackageManager,
   getPackageManagerCommand,
-} from "./packageManager.js" // Import missing functions
-import useBlitzTemplate from "./useBlitzTemplate.js" // Import missing function
+} from "./packageManager.js";
+import useBlitzTemplate from "./useBlitzTemplate.js";
 
 async function blitz() {
   try {
     // Welcome message
-    console.log("Welcome to React Native Blitz!")
+    console.log("Welcome to React Native Blitz!");
 
     // Get project name from command line arguments
-    let projectName = process.argv[3] // Get the second argument after "init"
+    let projectName = process.argv[3];
 
     // If no project name is provided, ask for it
     if (!projectName) {
@@ -25,45 +25,58 @@ async function blitz() {
           name: "name",
           message: "Enter project name:",
         },
-      ])
-      projectName = name
+      ]);
+      projectName = name;
     }
 
     if (!projectName) {
-      console.error("Please specify the project name.")
-      return
+      console.error("Please specify the project name.");
+      return;
     }
 
     // Get project folder path
-    const projectFolderPath = path.join(process.cwd(), projectName)
+    const projectFolderPath = path.join(process.cwd(), projectName);
 
     // Ask user for preferred package manager
-    const packageManagerChoice = await askPackageManager()
-    const packageManagerCommand = getPackageManagerCommand(packageManagerChoice)
+    const packageManagerChoice = await askPackageManager();
+    const packageManagerCommand = getPackageManagerCommand(packageManagerChoice);
+
+    // Define the command based on the chosen package manager
+    let initializationCommand;
+    switch (packageManagerChoice) {
+      case "npm":
+        initializationCommand = "npx react-native@latest init";
+        break;
+      case "yarn":
+        initializationCommand = "yarn react-native@latest init";
+        break;
+      case "pnpm":
+        initializationCommand = "pnpx react-native@latest init";
+        break;
+      case "bun":
+        initializationCommand = "bunx react-native@latest init";
+        break;
+      default:
+        console.error("Invalid package manager choice:", packageManagerChoice);
+        return;
+    }
 
     // Run initialization command with the chosen package manager
-    execCommand(
-      `${packageManagerCommand} react-native@latest init ${projectName}`,
-    )
+    execCommand(`${packageManagerCommand} ${initializationCommand} ${projectName}`);
 
     // Ask user about setup options
-    const setupOptions = await askSetupOptions()
+    const setupOptions = await askSetupOptions();
 
     // Configure project based on user responses
-    await configureProject(
-      setupOptions,
-      projectFolderPath,
-      packageManagerChoice,
-    )
-    await useBlitzTemplate(projectFolderPath, packageManagerChoice)
+    await configureProject(setupOptions, projectFolderPath, packageManagerChoice);
+    await useBlitzTemplate(projectFolderPath, packageManagerChoice);
 
     console.log(
-      "Project setup completed successfully!\nPlease be sure to run pod install in the ios directory if you are developing for iOS as well",
-    )
+      "Project setup completed successfully!\nPlease be sure to run pod install in the ios directory if you are developing for iOS as well"
+    );
   } catch (error) {
-    // Other errors
-    console.error("Error occurred during project setup:", error)
+    console.error("Error occurred during project setup:", error);
   }
 }
 
-export default blitz
+export default blitz;
